182f5a2d41eeb3a35675344ded0c1f9c6f2ca455
zesterer <barry.of.smith@gmail.com>
2016-06-14 23:17:41 +0100

    Updated README and fixed grub.cfg

--- grub/create-iso.sh
+++ grub/create-iso.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+DATE=`date --date="now" "+%Y-%m-%d"`
+
 cp ../kernel/thoth.bin isodir/boot/thoth.bin
 cp grub.cfg isodir/boot/grub/grub.cfg
 grub-mkrescue -o thoth.iso isodir
+#cp thoth.iso thoth-${DATE}.iso
